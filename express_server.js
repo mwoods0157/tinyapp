a2c473d094e0227d75e22185d0b06b09aec3d3de
matthew woods <matthew.woods0157@gmail.com>
2021-12-06 20:02:53 +0000

    Added the app.set

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -32,3 +34,6 @@ app.get("/fetch", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+//http://localhost:8080/set
+//http://localhost:8080/fetch
