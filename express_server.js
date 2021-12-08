b87a06a5e56daa2c4783140b8313002cab3d2dee
matthew woods <matthew.woods0157@gmail.com>
2021-12-08 14:51:51 +0000

    Adding login/logout logic to express server

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+var cookies = require("cookie-parser");
+app.use(cookies());
 
 app.set("view engine", "ejs");
 
@@ -21,6 +23,8 @@ function generateRandomString() {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -33,17 +37,30 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//Added displaying the username
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  console.log(req.cookies);
+  const templateVars = { 
+      urls: urlDatabase, 
+      username: req.cookies["username"]
+    };
   res.render("urls_index", templateVars);
 });
 
+//Added displaying username
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+    const templateVars = {
+        username: req.cookies['username']
+    }
+    res.render("urls_new", templateVars);
 });
 
+//Added displaying username
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL], 
+    username: req.cookies['username'] };
   res.render("urls_show", templateVars);
 });
 
@@ -68,6 +85,11 @@ app.post("/urls/:shortURL", (req, res) => {
 
 app.post("/login", (req, res) => {
     res.cookie('username', req.body.username);
+    res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+    res.clearCookie('username', req.body.username);
     res.redirect('/urls');
 })
 
@@ -97,11 +119,14 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
 });
 
-const templateVars = {
+/*const templateVars = {
     username: req.cookies["username"],
     // ... any other vars
 };
 res.render("urls_index", templateVars);
+res.render("urls_show", templateVars);
+res.render("urls_new", templateVars);
+res.render("_header.ejs", templateVars);
 
 //http://localhost:8080/set
-//http://localhost:8080/fetch
+//http://localhost:8080/fetch */
